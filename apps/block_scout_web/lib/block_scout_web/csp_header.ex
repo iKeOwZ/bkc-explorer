ad35088edef56b80d399d2176a73953c8888f588
Siwapon Srifa <keow.siwapon@bitkub.com>
2021-06-16 19:28:40 +0700

    first commit

---
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -0,0 +1,29 @@
+defmodule BlockScoutWeb.CSPHeader do
+  @moduledoc """
+  Plug to set content-security-policy with websocket endpoints
+  """
+
+  alias Phoenix.Controller
+  alias Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    Controller.put_secure_browser_headers(conn, %{
+      "content-security-policy" => "\
+        connect-src 'self' #{websocket_endpoints(conn)} *.googletagmanager.com *.google-analytics.com; \
+        default-src 'self' ;\
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' *.googletagmanager.com *.google-analytics.com https://www.googletagmanager.com/gtag/js https://www.google-analytics.com/analytics.js ;\
+        style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com;\
+        img-src 'self' * data:;\
+        media-src 'self' * data:;\
+        font-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.gstatic.com data:;\
+      "
+    })
+  end
+
+  defp websocket_endpoints(conn) do
+    host = Conn.get_req_header(conn, "host")
+    "ws://#{host} wss://#{host}"
+  end
+end
---
+++ env_mainnet.sh
@@ -0,0 +1,15 @@
+export LOGO=/images/bitkubchain-logo.png
+export ETHEREUM_JSONRPC_VARIANT=geth
+export ETHEREUM_JSONRPC_HTTP_URL=http://159.138.245.69:8545
+export ETHEREUM_JSONRPC_WS_URL=ws://159.138.245.69:8546
+export ETHEREUM_JSONRPC_TRACE_URL=http://159.138.245.69:8545
+export BLOCKSCOUT_HOST=bkcscan.com
+export BLOCKSCOUT_PROTOCOL=https
+export NETWORK="Mainnet"
+export SUBNETWORK="Bitkub Chain"
+export BLOCK_TRANSFORMER=clique
+export DISABLE_EXCHANGE_RATES=true
+export ENABLE_GOOGLE_ANALYTICS=true
+export ENABLE_TXS_STATS=true
+export SHOW_PRICE_CHART=true
+export SHOW_TXS_CHART=true
